f6a66a41c7220c7fce0d9b856dccb10e0bfb8145
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-03-16 19:26:36 -0400

    added javascript elements to call on the api

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,13 @@
-var APIKey = "8b47a2778fecabccd23cc155ce9bf82f"
+const APIKey = "8b47a2778fecabccd23cc155ce9bf82f"
+const searchBtn = document.getElementById(`search-btn`)
+const cityName = document.getElementById(`city-name`)
+
+searchBtn.addEventListener(`click` , () => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName.value}&limit=1&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(data => 
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&cnt=6&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        )
+})
